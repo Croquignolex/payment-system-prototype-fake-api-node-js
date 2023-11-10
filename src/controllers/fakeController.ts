3fd23976a9f53373dc3241ce792e6e95bc3112dc
Croquignolex <gpetitalex10@gmail.com>
2023-11-10 08:27:25 +0100

    Follow up front end

--- src/controllers/fakeController.ts
+++ src/controllers/fakeController.ts
@@ -1,21 +1,39 @@
 import {Request, Response} from 'express';
 
-/*
-export const login = async (req: Request, res: Response): Promise<Response> => {
-    return res.send({
-        firstName: "Alex",
-        lastName: "NGOMBOL",
-        email: "gpetitalex10@gmail.com",
-        accountId: "73d5e89c-a221-4876-9ac9-09b7bcf2ec29"
-    });
-};
-*/
-
 export const register = async (req: Request, res: Response): Promise<Response> => {
-    // return res.status(400).send();
     return res.send({accountId: "73d5e89c-a221-4876-9ac9-09b7bcf2ec29"});
 };
 
-export const logout = async (res: Response): Promise<Response>  => {
+export const accountInfo = async (req: Request, res: Response): Promise<Response> => {
+    // return res.status(400).send();
+    return res.send({
+        userName: "croquignolex",
+        firstName: "Croquignolex",
+        lastName: "NGOMBOL",
+        emailAddress: "gpetitalex10@gmail.com",
+        phoneNumber: "+237673848464",
+        countryCode: "CM",
+        accountId: "73d5e89c-a221-4876-9ac9-09b7bcf2ec29",
+        address: {
+            city: "Douala",
+            zipCode: "144",
+            street: "Ndogbong",
+            country: "CAMEROUN",
+        },
+    });
+};
+
+export const accountAddressUpdate = async (req: Request, res: Response): Promise<Response> => {
+    // return res.status(400).send();
+    return res.send();
+};
+
+export const contactAdd = async (req: Request, res: Response): Promise<Response> => {
+    // return res.status(400).send();
+    return res.send();
+};
+
+export const accountAdd = async (req: Request, res: Response): Promise<Response> => {
+    // return res.status(400).send();
     return res.send();
 };
--- src/routes/fakeRoute.ts
+++ src/routes/fakeRoute.ts
@@ -1,10 +1,13 @@
 import express, {Router} from 'express';
 
-import {register} from "../controllers/fakeController";
+import {register, accountInfo, accountAddressUpdate, contactAdd, accountAdd} from "../controllers/fakeController";
 
 const router: Router = express.Router();
 
-// router.post('/login', login);
 router.post('/account/register/email', register);
+router.get('/account/:accountId', accountInfo);
+router.put('/account/:accountId/addaddress', accountAddressUpdate);
+router.put('/account/:accountId/transferrecipient', contactAdd);
+router.put('/account/:accountId/transferpayer', accountAdd);
 
 export default router;
