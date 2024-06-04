d0f063df35c731474fdb6d3320d6ac5836983ee5
Croquignolex <gpetitalex10@gmail.com>
2024-06-04 10:21:51 +0100

    Update Api

--- src/fakeController.ts
+++ src/fakeController.ts
@@ -92,7 +92,7 @@ export const transferAddMtnToMtn = async (req: Request, res: Response): Promise<
 
     transfersData.push(transfer);
 
-    return res.send({id: transferId, message: ""});
+    return res.send({transferId, message: ""});
 };
 
 export const transferStatus = async (req: Request, res: Response): Promise<Response> => {
